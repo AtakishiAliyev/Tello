e84672bb931faf800ec171263ccc7f78224fb2b2
Atakishi <atakishieliyev@gmail.com>
2022-06-08 17:43:07 +0400

    Project setup done

---
+++ src/container/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.scss'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='title'>Hello world</h1>
+        </div>
+    )
+}
+
+export default Home
