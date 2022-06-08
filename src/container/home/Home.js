b615ed566bf07fb8c23d98b4e76b062126fb47bf
Atakishi <atakishieliyev@gmail.com>
2022-06-08 18:44:17 +0400

    Done

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 import './Home.scss'
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <h1 className='title'>Hello world</h1>
         </div>
     )
