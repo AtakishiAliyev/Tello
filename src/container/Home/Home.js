5c0bf3b05c51628cad7f1d1920d9e6e703284e78
Atakishi <atakishieliyev@gmail.com>
2022-06-16 13:42:34 +0400

    clear

---
+++ src/container/Home/Home.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import './Home.scss'
+import axios from 'axios'
+import Header from '../../components/Header/Header'
+import Slider from '../../components/Slider/Slider'
+import ProductsSlider from '../../components/ProductsSlider/ProductsSlider'
+
+const Home = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        async function getData() {
+            const url = new URL(
+                "https://api.chec.io/v1/categories"
+            );
+
+            let params = {
+                "depth": "3",
+            };
+
+            Object.keys(params)
+                .forEach(key => url.searchParams.append(key, params[key]));
+
+            let headers = {
+                "X-Authorization": "pk_437901876da421fb3e83a2493acac7205e61388d27e32",
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            };
+
+            const response = await axios.get(url, {
+                method: "GET",
+                headers: headers,
+            })
+
+            response.data.data.map(item => {
+                setCategories(item.children)
+                return false
+            })
+        }
+
+        getData()
+    }, [])
+
+    return (
+        <>
+            <Header categories={categories} />
+            <Slider />
+            <ProductsSlider />
+            <ProductsSlider />
+            <ProductsSlider />
+        </>
+    )
+}
+
+export default Home
