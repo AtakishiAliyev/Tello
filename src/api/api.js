39ea1634d8f21c846abc4ad703e10b4ef2100ebe
Atakishi <atakishieliyev@gmail.com>
2022-07-12 17:47:39 +0400

    Clear

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const { REACT_APP_API_KEY } = process.env
 const instance = axios.create({
     baseURL: 'https://api.chec.io/v1',
     headers: {
-        "X-Authorization": REACT_APP_API_KEY,
+        "X-Authorization": 'pk_437901876da421fb3e83a2493acac7205e61388d27e32',
         "Accept": "application/json",
         "Content-Type": "application/json",
     }
